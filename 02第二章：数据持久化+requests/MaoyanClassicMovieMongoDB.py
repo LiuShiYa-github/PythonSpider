07ee5f6f8296dcec1ee46c0fcf822364ca7a63c6
shiya.liu <2573514647@qq.com>
2022-04-30 21:50:56 +0800

    Merge remote-tracking branch 'origin/master'

--- 02第二章：数据持久化+requests/MaoyanClassicMovieMongoDB.py
+++ 02第二章：数据持久化+requests/MaoyanClassicMovieMongoDB.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-@FileName: MaoyanClassicFile.py
+@FileName: MaoyanClassicMovieMongoDB.py
 @Time    : 2022/4/10 22:31
 @Author  : 热气球
 @Software: PyCharm
@@ -22,7 +22,7 @@ class MaoyanSpider_Classic_Film:
         self.url = 'https://www.maoyan.com/films?showType=3&sortId=1&offset={}'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0',
-            'Cookie': 'uuid_n_v=v1; uuid=3AE37E108CA611EC8FDC8396A0F7AFFCD945C250A076432C8AFD89FCB0E193D7; _csrf=ebdd97cd428809914f8919dcfe0c1031f72c3caf9b01aa03d9c831fae4dffd7f; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1644740619; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1644765969; _lxsdk_cuid=17ef22e2f1fc8-0bb075b44e2f43-4c3e237c-144000-17ef22e2f1f27; _lxsdk=3AE37E108CA611EC8FDC8396A0F7AFFCD945C250A076432C8AFD89FCB0E193D7; __mta=150189705.1644740620254.1644765647349.1644765969306.80; _lxsdk_s=17ef39966be-c29-c61-4a0%7C%7C43'
+            'Cookie': '__mta=150960649.1645347489665.1648359488006.1648359602267.36; _lxsdk_cuid=17f16596927c8-0466fea13319f6-576153e-144000-17f16596927c8; uuid_n_v=v1; uuid=40676940C88411ECB7121591F0C657D661F33FA860EA4951B337B0151B32BBDE; _csrf=a470cd2799dd3a8cced04ed718e25a055de1dc9e07c539ec0a7a2292a8d0711c; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1651323092; _lxsdk=40676940C88411ECB7121591F0C657D661F33FA860EA4951B337B0151B32BBDE; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1651323108; __mta=150960649.1645347489665.1648359602267.1651323108420.37; _lxsdk_s=1807a86be77-29-ab5-4c5%7C%7C6'
         }
         self.i = 0
         self.conn = pymongo.MongoClient(host='10.0.0.101', port=27017)
@@ -30,7 +30,7 @@ class MaoyanSpider_Classic_Film:
         self.myset = self.db['maoyanset']
 
     def get_html(self, url):
-        "获取HTML内容"
+        """获取HTML内容"""
         req = request.Request(url=url, headers=self.headers)
         res = request.urlopen(req)
         html = res.read().decode()
@@ -39,7 +39,7 @@ class MaoyanSpider_Classic_Film:
         self.parse_html(html)
 
     def parse_html(self, html):
-        "提取HTML内容"
+        """提取HTML内容"""
         regex = '<div class="movie-hover-info">.*?<span class="name ">(.*?)</span>.*?<span class="score channel-detail-orange"><i class="integer">(.*?)</i><i class="fraction">(.*?)</i></span>.*?<span class="hover-tag">类型:</span>(.*?)</div>.*?<span class="hover-tag">主演:</span>(.*?)</div>.*?<span class="hover-tag">上映时间:</span>(.*?)</div>.*?</div>'
         pattern = re.compile(regex, re.S)
         r_list = pattern.findall(html)
@@ -60,7 +60,7 @@ class MaoyanSpider_Classic_Film:
             self.i += 1
 
     def run(self):
-        "程序运行调配"
+        """程序运行调配"""
         for page in range(0, 91, 10):
             self.get_html(url=self.url.format(page * 30))
             time.sleep(random.randint(1, 2))
